3f99dc8d23fd2c68dc544694a91fb58fced053e3
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-01-29 15:03:17 +0600

    decided tasks on code.mu

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const arr = [1, 2, 3, 4, 5];
+let obj;
 
 export default {
 	'/Calc/': function () {
@@ -19,9 +20,31 @@ export default {
 		};
 		return res;
 	},
-	'/handler/': function (data) {
-		console.log(data);
-		return `data is resived`;
+	'/handler/': function ({ get }) {
+		return Number(get.numOne) + Number(get.numTwo);
 	},
+	'/average/': function (nums) {
+		let sum = 0;
+		console.log(nums);
+		for (let key in nums.get) {
+			sum += Number(nums.get[key]);
+		};
+		return sum;
+	},
+	'/authorization/': function ({ get }) {
+		console.log(get);
+		if (get.name && get.surname) {
+			return 'data is resived';
+		} else {
+			return 'data is not full';
+		}
+	},
+	'/formDate/': function ({ get }) {
+		console.log(get.date);
+		if (/\d{4}\.\d{2}\.\d{2}$/.test(get.date)) {
+			return 'date is correct'
+		} else {
+			return 'date is not correct'
+		}
+	}
 };
-
